8fc338e100b570dfe9352c5534cb6f0e81911e2b
Leonardo Braz <lhleonardo05@gmail.com>
2020-07-15 23:39:39 -0300

    Resolvi o mistério da fé

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,5 +1,7 @@
-export const formatValue = (value: number): string =>
+const formatValue = (value: number): string =>
   Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
   }).format(value);
+
+export default formatValue;
